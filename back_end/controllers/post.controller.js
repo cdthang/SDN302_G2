750b494a67220a0809368f71f003d38aefa76fe8
sonhonggiang <songsonpro247@gmail.com>
2026-03-11 11:49:34 +0700

    Add backend marketplace module

--- back_end/controllers/post.controller.js
+++ back_end/controllers/post.controller.js
@@ -12,8 +12,23 @@ export const createPost = async (req, res) => {
 
 export const getPosts = async (req, res) => {
   try {
-    const posts = await Post.find({ status: "approved" });
+
+    const { search, category } = req.query;
+
+    let query = {};
+
+    if (search) {
+      query.title = { $regex: search, $options: "i" };
+    }
+
+    if (category) {
+      query.category = category;
+    }
+
+    const posts = await Post.find(query);
+
     res.json(posts);
+
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -26,4 +41,50 @@ export const getPostById = async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
+
+};
+export const updatePost = async (req, res) => {
+  try {
+    const post = await Post.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+    res.json(post);
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+export const deletePost = async (req, res) => {
+  try {
+
+    const post = await Post.findByIdAndDelete(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+    res.json({ message: "Post deleted" });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const approvePost = async (req, res) => {
+
+  const post = await Post.findByIdAndUpdate(
+    req.params.id,
+    { status: "approved" },
+    { new: true }
+  );
+
+  res.json(post);
+
 };
