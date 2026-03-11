a2c2c683cea186a123bd72665eddde961b4494ec
sonhonggiang <songsonpro247@gmail.com>
2026-03-11 13:56:15 +0700

    Add backend API

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,10 +1,20 @@
 import express from "express";
-import { createPost, getPosts, getPostById } from "../controllers/post.controller.js";
+import {
+  createPost,
+  getPosts,
+  getPostById,
+  updatePost,
+  deletePost,
+  approvePost
+} from "../controllers/post.controller.js";
+
+import { upload } from "../middleware/upload.js";
 
 const router = express.Router();
-
-router.post("/", createPost);
+router.post("/", upload.single("image"), createPost);
 router.get("/", getPosts);
 router.get("/:id", getPostById);
-
+router.put("/:id", updatePost);
+router.delete("/:id", deletePost);
+router.patch("/:id/approve", approvePost);
 export default router;
